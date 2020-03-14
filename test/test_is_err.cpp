cafb08d8a385513459a9fc933b2d8fb81d92ca9d
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-13 22:15:52 -0500

    UPdated tests

---
+++ test/test_is_err.cpp
@@ -0,0 +1,17 @@
+#include <doctest.hpp>
+#include <result/result.hpp>
+using namespace result;
+
+using doctest::test_suite;
+
+TEST_CASE("Returns true if the result is Err." *
+          test_suite("is_err")) {
+  Result<int, std::string> x = Err(std::string{"Some error message"});
+  REQUIRE(x.is_err());
+}
+
+TEST_CASE("Returns true if the result is Err." *
+          test_suite("is_err")) {
+  Result<int, std::string> x = Ok(-3);
+  REQUIRE_FALSE(x.is_err());
+}
