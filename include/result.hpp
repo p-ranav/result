0caea1eebe89a6c800bbc5d4bd16d7987a61dd54
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-13 21:59:15 -0500

    Clang format

--- include/result.hpp
+++ include/result.hpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <variant>
-#include <optional>
 #include <exception>
+#include <iostream>
+#include <optional>
+#include <variant>
 
 namespace result {
 
@@ -16,8 +16,7 @@ template <typename E> struct Err {
 };
 
 // Result is a type that represents either success (Ok) or failure (Err).
-template <typename T, typename E>
-struct Result {
+template <typename T, typename E> struct Result {
 
   std::variant<Ok<T>, Err<E>> value;
 
@@ -34,9 +33,7 @@ struct Result {
     return *this;
   }
 
-  bool operator==(const Ok<T> &val) {
-    return is_ok() && unwrap() == val.value;
-  }
+  bool operator==(const Ok<T> &val) { return is_ok() && unwrap() == val.value; }
 
   bool operator==(const Err<E> &val) {
     return is_err() && unwrap_err() == val.value;
@@ -52,14 +49,14 @@ struct Result {
   std::optional<E> err() const { return std::get<1>(value).value; }
 
   // Returns res if the result is Ok, otherwise returns the Err value of self.
-  Result and_(const Result<T, E>& res) {
+  Result and_(const Result<T, E> &res) {
     if (is_ok())
       return res;
     return Err(err().value());
   }
 
   // Synonymous with Result.and_(res)
-  Result operator&&(const Result<T, E>& res) {
+  Result operator&&(const Result<T, E> &res) {
     if (is_ok())
       return res;
     return Err(err().value());
@@ -67,32 +64,30 @@ struct Result {
 
   // Calls op if the result is Ok, otherwise returns the Err value of self.
   // This function can be used for control flow based on Result values.
-  template <typename Function>
-  Result and_then(Function op) {
+  template <typename Function> Result and_then(Function op) {
     if (is_ok())
       return op(unwrap());
     return Err(err().value());
   }
 
-  // Returns res if the result is Err, 
+  // Returns res if the result is Err,
   // otherwise returns the Ok value of self.
-  Result or_(const Result<T, E>& res) {
+  Result or_(const Result<T, E> &res) {
     if (is_err())
       return res;
     return Ok(ok().value());
   }
 
   // Synonymous with Result.or_(res)
-  Result operator||(const Result<T, E>& res) {
+  Result operator||(const Result<T, E> &res) {
     if (is_err())
       return res;
     return Ok(ok().value());
   }
 
-  // Calls op if the result is Err, 
+  // Calls op if the result is Err,
   // otherwise returns the Ok value of self.
-  template <typename Function>
-  Result or_else(Function op) {
+  template <typename Function> Result or_else(Function op) {
     if (is_err())
       return op(unwrap_err());
     return Ok(ok().value());
@@ -105,10 +100,9 @@ struct Result {
     return optb;
   }
 
-  // Unwraps a result, yielding the content of an Ok. 
+  // Unwraps a result, yielding the content of an Ok.
   // If the value is an Err then it calls op with its value.
-  template <typename Function>
-  T unwrap_or_else(Function op) {
+  template <typename Function> T unwrap_or_else(Function op) {
     if (is_ok())
       return unwrap();
     return op(unwrap_err());
@@ -122,20 +116,20 @@ struct Result {
     return is_err() ? unwrap_err() == this_value : false;
   }
 
-  // Maps a Result<T, E> to Result<U, E> by 
-  // applying a function to a contained Ok value, 
+  // Maps a Result<T, E> to Result<U, E> by
+  // applying a function to a contained Ok value,
   // leaving an Err value untouched.
   //
   // This function can be used to compose the results of two functions.
   template <typename Function>
   auto map(Function fn) -> Result<decltype(fn(T())), E> {
     if (is_ok())
-      return Result<decltype(fn(T())), E>(Ok<decltype(fn(T()))>(fn(unwrap()))); 
+      return Result<decltype(fn(T())), E>(Ok<decltype(fn(T()))>(fn(unwrap())));
     else
       return Result<decltype(fn(T())), E>(Err<E>(unwrap_err()));
   }
 
-  // Applies a function to the contained value (if any), 
+  // Applies a function to the contained value (if any),
   // or returns the provided default (if not).
   template <typename Value, typename Function>
   auto map_or(Value default_value, Function fn) -> decltype(fn(T())) {
@@ -145,35 +139,38 @@ struct Result {
       return default_value;
   }
 
-  // Maps a Result<T, E> to U by applying a function to a contained Ok value, 
+  // Maps a Result<T, E> to U by applying a function to a contained Ok value,
   // or a fallback function to a contained Err value.
   //
-  // This function can be used to unpack a successful result while handling an error.
+  // This function can be used to unpack a successful result while handling an
+  // error.
   template <typename ErrorFunction, typename OkFunction>
-  auto map_or_else(ErrorFunction err_fn, OkFunction ok_fn) -> decltype(ok_fn(T())) {
+  auto map_or_else(ErrorFunction err_fn, OkFunction ok_fn)
+      -> decltype(ok_fn(T())) {
     if (is_ok())
       return ok_fn(unwrap());
     else
       return err_fn(unwrap_err());
   }
 
-  // Maps a Result<T, E> to Result<T, F> by 
-  // applying a function to a contained Err value, 
+  // Maps a Result<T, E> to Result<T, F> by
+  // applying a function to a contained Err value,
   // leaving an Ok value untouched.
   //
-  // This function can be used to pass 
+  // This function can be used to pass
   // through a successful result while handling an error.
   template <typename Function>
   auto map_err(Function fn) -> Result<T, decltype(fn(E()))> {
     if (is_err())
-      return Result<T, decltype(fn(E()))>(Err<decltype(fn(E()))>(fn(unwrap_err()))); 
+      return Result<T, decltype(fn(E()))>(
+          Err<decltype(fn(E()))>(fn(unwrap_err())));
     else
       return Result<T, decltype(fn(E()))>(Ok<T>(unwrap()));
   }
 
   // Unwraps a result, yielding the content of an Ok.
   // Throws if the value is an Err
-  auto unwrap() const { 
+  auto unwrap() const {
     if (is_ok())
       return ok().value();
     else
@@ -193,7 +190,7 @@ struct Result {
   auto unwrap_err() const {
     if (is_err())
       return err().value();
-    else 
+    else
       throw ok().value();
   }
 
@@ -206,15 +203,14 @@ struct Result {
   }
 
   // Returns the contained value or a default
-  // if Ok, returns the contained value, 
+  // if Ok, returns the contained value,
   // otherwise if Err, returns the default value for that type.
   T unwrap_or_default() const {
     if (is_ok())
       return unwrap();
-    else 
+    else
       return T();
   }
-
 };
 
-}
+} // namespace result
