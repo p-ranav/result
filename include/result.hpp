9356cba9b4cfa012969dccb674b1f84bc623031f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-03-13 20:53:50 -0500

    Added .map_or_else() method

--- include/result.hpp
+++ include/result.hpp
@@ -61,6 +61,26 @@ struct Result {
     return err();
   }
 
+  // Unwraps a result, yielding the content of an Ok.
+  // Panics if the value is an Err, 
+  // with a panic message including the passed message, 
+  // and the content of the Err.
+  T expect(const std::string &msg) {
+    if (is_ok())
+      return unwrap();
+    throw msg + ": " + std::to_string(unwrap_err());
+  }
+  
+  // Unwraps a result, yielding the content of an Err.
+  // Panics if the value is an Ok, 
+  // with a panic message including the passed message, 
+  // and the content of the Ok.
+  E expect_err(const std::string &msg) {
+    if (is_err())
+      return unwrap_err();
+    throw msg + ": " + std::to_string(unwrap());
+  }
+
   auto unwrap() const { 
     if (is_ok())
       return ok().value;
@@ -75,6 +95,16 @@ struct Result {
       throw ok().value;
   }
 
+  // Returns the contained value or a default
+  // if Ok, returns the contained value, 
+  // otherwise if Err, returns the default value for that type.
+  T unwrap_or_default() const {
+    if (is_ok())
+      return unwrap();
+    else 
+      return T();
+  }
+
   bool contains(const T &this_value) {
     return is_ok() ? unwrap() == this_value : false;
   }
@@ -83,6 +113,11 @@ struct Result {
     return is_err() ? unwrap_err() == this_value : false;
   }
 
+  // Maps a Result<T, E> to Result<U, E> by 
+  // applying a function to a contained Ok value, 
+  // leaving an Err value untouched.
+  //
+  // This function can be used to compose the results of two functions.
   template <typename Function>
   auto map(Function fn) -> Result<decltype(fn(T())), E> {
     if (is_ok())
@@ -91,12 +126,26 @@ struct Result {
       return Result<decltype(fn(T())), E>(Err<E>(unwrap_err()));
   }
 
+  // Applies a function to the contained value (if any), 
+  // or returns the provided default (if not).
   template <typename Value, typename Function>
   auto map_or(Value default_value, Function fn) -> decltype(fn(T())) {
     if (is_ok())
       return fn(unwrap());
     else
       return default_value;
+  }
+
+  // Maps a Result<T, E> to U by applying a function to a contained Ok value, 
+  // or a fallback function to a contained Err value.
+  //
+  // This function can be used to unpack a successful result while handling an error.
+  template <typename ErrorFunction, typename OkFunction>
+  auto map_or_else(ErrorFunction err_fn, OkFunction ok_fn) -> decltype(ok_fn(T())) {
+    if (is_ok())
+      return ok_fn(unwrap());
+    else
+      return err_fn(unwrap_err());
   }
   
 };
